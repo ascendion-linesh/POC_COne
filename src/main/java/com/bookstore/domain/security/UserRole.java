c325efc7e75be610067d20000a888a8245bb9ece
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-29 20:10:05 +0530

    Latest change - Critical security: UserRole entity with jakarta.persistence for user-role mapping

--- src/main/java/com/bookstore/domain/security/UserRole.java
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -1,13 +1,13 @@
 package com.bookstore.domain.security;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import com.bookstore.domain.User;
 
@@ -63,6 +63,4 @@ public class UserRole {
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
-	
 }
