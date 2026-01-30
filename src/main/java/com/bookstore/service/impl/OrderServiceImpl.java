ed8ca3c6125da1a53c7b99ef0648f734bd4b5025
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 14:53:49 +0530

    Remediated code change - Updated OrderServiceImpl with Optional handling for Spring Data JPA

--- src/main/java/com/bookstore/service/impl/OrderServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/OrderServiceImpl.java
@@ -21,48 +21,48 @@ import com.bookstore.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService{
-    
-    @Autowired
-    private OrderRepository orderRepository;
-    
-    @Autowired
-    private CartItemService cartItemService;
-    
-    public synchronized Order createOrder(ShoppingCart shoppingCart,
-            ShippingAddress shippingAddress,
-            BillingAddress billingAddress,
-            Payment payment,
-            String shippingMethod,
-            User user) {
-        Order order = new Order();
-        order.setBillingAddress(billingAddress);
-        order.setOrderStatus("created");
-        order.setPayment(payment);
-        order.setShippingAddress(shippingAddress);
-        order.setShippingMethod(shippingMethod);
-        
-        List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
-        
-        for(CartItem cartItem : cartItemList) {
-            Book book = cartItem.getBook();
-            cartItem.setOrder(order);
-            book.setInStockNumber(book.getInStockNumber() - cartItem.getQty());
-        }
-        
-        order.setCartItemList(cartItemList);
-        order.setOrderDate(Calendar.getInstance().getTime());
-        order.setOrderTotal(shoppingCart.getGrandTotal());
-        shippingAddress.setOrder(order);
-        billingAddress.setOrder(order);
-        payment.setOrder(order);
-        order.setUser(user);
-        order = orderRepository.save(order);
-        
-        return order;
-    }
-    
-    public Optional<Order> findById(Long id) {
-        return orderRepository.findById(id);
-    }
-
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CartItemService cartItemService;
+	
+	public synchronized Order createOrder(ShoppingCart shoppingCart,
+			ShippingAddress shippingAddress,
+			BillingAddress billingAddress,
+			Payment payment,
+			String shippingMethod,
+			User user) {
+		Order order = new Order();
+		order.setBillingAddress(billingAddress);
+		order.setOrderStatus("created");
+		order.setPayment(payment);
+		order.setShippingAddress(shippingAddress);
+		order.setShippingMethod(shippingMethod);
+		
+		List<CartItem> cartItemList = cartItemService.findByShoppingCart(shoppingCart);
+		
+		for(CartItem cartItem : cartItemList) {
+			Book book = cartItem.getBook();
+			cartItem.setOrder(order);
+			book.setInStockNumber(book.getInStockNumber() - cartItem.getQty());
+		}
+		
+		order.setCartItemList(cartItemList);
+		order.setOrderDate(Calendar.getInstance().getTime());
+		order.setOrderTotal(shoppingCart.getGrandTotal());
+		shippingAddress.setOrder(order);
+		billingAddress.setOrder(order);
+		payment.setOrder(order);
+		order.setUser(user);
+		order = orderRepository.save(order);
+		
+		return order;
+	}
+	
+	public Order findOne(Long id) {
+		Optional<Order> orderOptional = orderRepository.findById(id);
+		return orderOptional.orElse(null);
+	}
 }
