7a83fceb6b796af5e4f5925458b7112277e27abb
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 18:17:37 +0530

    Latest Code changes

--- src/main/java/com/bookstore/domain/BookToCartItem.java
+++ src/main/java/com/bookstore/domain/BookToCartItem.java
@@ -1,11 +1,11 @@
 package com.bookstore.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class BookToCartItem {
@@ -45,6 +45,4 @@ public class BookToCartItem {
 	public void setCartItem(CartItem cartItem) {
 		this.cartItem = cartItem;
 	}
-	
-	
 }
