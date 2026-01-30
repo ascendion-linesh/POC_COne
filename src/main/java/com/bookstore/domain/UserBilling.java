fe0b9c849ff473e298de96968187085261a4af3c
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 18:18:46 +0530

    Latest Code changes

--- src/main/java/com/bookstore/domain/UserBilling.java
+++ src/main/java/com/bookstore/domain/UserBilling.java
@@ -1,11 +1,11 @@
 package com.bookstore.domain;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class UserBilling {
@@ -95,6 +95,4 @@ public class UserBilling {
 	public void setUserPayment(UserPayment userPayment) {
 		this.userPayment = userPayment;
 	}
-	
-	
 }
