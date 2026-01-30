bc893f871d24ee0a4fa6b3e94f603ec3d9389508
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 18:17:46 +0530

    Latest Code changes

--- src/main/java/com/bookstore/domain/CartItem.java
+++ src/main/java/com/bookstore/domain/CartItem.java
@@ -3,14 +3,14 @@ package com.bookstore.domain;
 import java.math.BigDecimal;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -93,6 +93,4 @@ public class CartItem {
 	public void setOrder(Order order) {
 		this.order = order;
 	}
-	
-	
 }
