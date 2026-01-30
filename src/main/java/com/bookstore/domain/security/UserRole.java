35349f1416facee615e5e4e594fbbd37f3c9c0a6
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 18:19:31 +0530

    Latest Code changes

--- src/main/java/com/bookstore/domain/security/UserRole.java
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -1,13 +1,13 @@
 package com.bookstore.domain.security;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import com.bookstore.domain.User;
 
@@ -34,35 +34,27 @@ public class UserRole {
 		this.role = role;
 	}
 
-
 	public Long getUserRoleId() {
 		return userRoleId;
 	}
-
 
 	public void setUserRoleId(Long userRoleId) {
 		this.userRoleId = userRoleId;
 	}
 
-
 	public User getUser() {
 		return user;
 	}
-
 
 	public void setUser(User user) {
 		this.user = user;
 	}
 
-
 	public Role getRole() {
 		return role;
 	}
 
-
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
-	
 }
