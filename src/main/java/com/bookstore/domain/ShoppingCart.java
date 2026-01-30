c085f159ad7c0b6d7424cf17f1fe072750b748ed
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 20:04:15 +0530

    Code Change: Migrate ShoppingCart entity to jakarta.persistence namespace

--- src/main/java/com/bookstore/domain/ShoppingCart.java
+++ src/main/java/com/bookstore/domain/ShoppingCart.java
@@ -16,48 +16,48 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class ShoppingCart {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private BigDecimal GrandTotal;
-	
-	@OneToMany(mappedBy="shoppingCart", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
-	@JsonIgnore
-	private List<CartItem> cartItemList;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	private User user;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private BigDecimal GrandTotal;
+    
+    @OneToMany(mappedBy="shoppingCart", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+    @JsonIgnore
+    private List<CartItem> cartItemList;
+    
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public BigDecimal getGrandTotal() {
-		return GrandTotal;
-	}
+    public BigDecimal getGrandTotal() {
+        return GrandTotal;
+    }
 
-	public void setGrandTotal(BigDecimal grandTotal) {
-		GrandTotal = grandTotal;
-	}
+    public void setGrandTotal(BigDecimal grandTotal) {
+        GrandTotal = grandTotal;
+    }
 
-	public List<CartItem> getCartItemList() {
-		return cartItemList;
-	}
+    public List<CartItem> getCartItemList() {
+        return cartItemList;
+    }
 
-	public void setCartItemList(List<CartItem> cartItemList) {
-		this.cartItemList = cartItemList;
-	}
+    public void setCartItemList(List<CartItem> cartItemList) {
+        this.cartItemList = cartItemList;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
