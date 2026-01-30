7e147966116059790119a2402224fb5a87cf67a2
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 16:06:37 +0530

    Code change to remediate - Update repository to use Optional and modern Spring Data JPA

--- src/main/java/com/bookstore/repository/UserRepository.java
+++ src/main/java/com/bookstore/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.bookstore.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
 import com.bookstore.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
-	User findByUsername(String username);
-	
-	User findByEmail(String email);
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
