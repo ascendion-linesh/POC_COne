96a7560a2bf5f35eac550eb9885ba30711ec9962
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 18:17:57 +0530

    Latest Code changes

--- src/main/java/com/bookstore/domain/Order.java
+++ src/main/java/com/bookstore/domain/Order.java
@@ -4,15 +4,15 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name="user_order")
@@ -113,8 +113,6 @@ public class Order {
 	public void setPayment(Payment payment) {
 		this.payment = payment;
 	}
-	
-	
 
 	public BillingAddress getBillingAddress() {
 		return billingAddress;
@@ -131,6 +129,4 @@ public class Order {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
 }
