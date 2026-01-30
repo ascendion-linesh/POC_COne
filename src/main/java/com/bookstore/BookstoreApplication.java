673a6ab474edbc50bac33d52c12549dc0e8eae05
ascendion-linesh <linesh.mishra@ascendion.com>
2026-01-30 19:45:17 +0530

    Latest Remediated Code Changes - Updated BookstoreApplication.java with Spring Boot 3.x compatibility

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -1,39 +1,12 @@
 package com.bookstore;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.bookstore.domain.User;
-import com.bookstore.service.UserService;
-import com.bookstore.utility.SecurityUtility;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-	
-	@Autowired
-	private UserService userService;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		User user1 = new User();
-		user1.setFirstName("John");
-		user1.setLastName("Adams");
-		user1.setUsername("j");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		user1.setEmail("JAdams@gmail.com");
-		
-		Set<String> roleNames = new HashSet<>();
-		roleNames.add("ROLE_USER");
-		
-		userService.createUser(user1, roleNames);
 	}
 }
