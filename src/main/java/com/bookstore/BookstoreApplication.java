9c7c1055faeb0d854873c84eac33eaf99732fee2
ascendion-linesh <linesh.mishra@ascendion.com>
2026-02-04 12:27:28 +0530

    Latest remediated code commit - BookstoreApplication.java: Add user existence check before creation

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -16,28 +16,33 @@ import com.bookstore.utility.SecurityUtility;
 
 @SpringBootApplication
 public class BookstoreApplication implements CommandLineRunner {
-	
-	@Autowired
-	private UserService userService;
+    
+    @Autowired
+    private UserService userService;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
-	}
-	
-	@Override
-	public void run(String... args) throws Exception {
-		User user1 = new User();
-		user1.setFirstName("John");
-		user1.setLastName("Adams");
-		user1.setUsername("j");
-		user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-		user1.setEmail("JAdams@gmail.com");
-		Set<UserRole> userRoles = new HashSet<>();
-		Role role1= new Role();
-		role1.setRoleId(1);
-		role1.setName("ROLE_USER");
-		userRoles.add(new UserRole(user1, role1));
-		
-		userService.createUser(user1, userRoles);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreApplication.class, args);
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+        User existingUser = userService.findByUsername("j");
+        
+        if (existingUser == null) {
+            User user1 = new User();
+            user1.setFirstName("John");
+            user1.setLastName("Adams");
+            user1.setUsername("j");
+            user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+            user1.setEmail("JAdams@gmail.com");
+            
+            Set<UserRole> userRoles = new HashSet<>();
+            Role role1 = new Role();
+            role1.setRoleId(1);
+            role1.setName("ROLE_USER");
+            userRoles.add(new UserRole(user1, role1));
+            
+            userService.createUser(user1, userRoles);
+        }
+    }
 }
