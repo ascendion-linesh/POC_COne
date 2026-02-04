65c40f459e993864d3ab78f7ef2c0b3608eb0926
ascendion-linesh <linesh.mishra@ascendion.com>
2026-02-04 12:27:37 +0530

    Latest remediated code commit - BookServiceImpl.java: Replace deprecated findOne() with findById() and Optional handling

--- src/main/java/com/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/BookServiceImpl.java
@@ -2,6 +2,7 @@ package com.bookstore.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,50 +13,50 @@ import com.bookstore.service.BookService;
 
 @Service
 public class BookServiceImpl implements BookService{
-	@Autowired
-	private BookRepository bookRepository;
-	
-	public List<Book> findAll() {
-		List<Book> bookList = (List<Book>) bookRepository.findAll();
-		List<Book> activeBookList = new ArrayList<>();
-		
-		for (Book book: bookList) {
-			if(book.isActive()) {
-				activeBookList.add(book);
-			}
-		}
-		
-		return activeBookList;
-	}
-	
-	public Book findOne(Long id) {
-		return bookRepository.findOne(id);
-	}
+    @Autowired
+    private BookRepository bookRepository;
+    
+    public List<Book> findAll() {
+        List<Book> bookList = (List<Book>) bookRepository.findAll();
+        List<Book> activeBookList = new ArrayList<>();
+        
+        for (Book book: bookList) {
+            if(book.isActive()) {
+                activeBookList.add(book);
+            }
+        }
+        
+        return activeBookList;
+    }
+    
+    public Book findOne(Long id) {
+        Optional<Book> bookOptional = bookRepository.findById(id);
+        return bookOptional.orElse(null);
+    }
 
-	public List<Book> findByCategory(String category){
-		List<Book> bookList = bookRepository.findByCategory(category);
-		
-		List<Book> activeBookList = new ArrayList<>();
-		
-		for (Book book: bookList) {
-			if(book.isActive()) {
-				activeBookList.add(book);
-			}
-		}
-		
-		return activeBookList;
-	}
-	
-	public List<Book> blurrySearch(String title) {
-		List<Book> bookList = bookRepository.findByTitleContaining(title);
-List<Book> activeBookList = new ArrayList<>();
-		
-		for (Book book: bookList) {
-			if(book.isActive()) {
-				activeBookList.add(book);
-			}
-		}
-		
-		return activeBookList;
-	}
+    public List<Book> findByCategory(String category){
+        List<Book> bookList = bookRepository.findByCategory(category);
+        List<Book> activeBookList = new ArrayList<>();
+        
+        for (Book book: bookList) {
+            if(book.isActive()) {
+                activeBookList.add(book);
+            }
+        }
+        
+        return activeBookList;
+    }
+    
+    public List<Book> blurrySearch(String title) {
+        List<Book> bookList = bookRepository.findByTitleContaining(title);
+        List<Book> activeBookList = new ArrayList<>();
+        
+        for (Book book: bookList) {
+            if(book.isActive()) {
+                activeBookList.add(book);
+            }
+        }
+        
+        return activeBookList;
+    }
 }
