33db23917130cbabdcd81e508170bfea855d3566
ascendion-linesh <linesh.mishra@ascendion.com>
2026-02-04 12:26:44 +0530

    Latest remediated code commit

--- src/main/java/com/bookstore/utility/SecurityUtility.java
+++ src/main/java/com/bookstore/utility/SecurityUtility.java
@@ -1,30 +1,48 @@
 package com.bookstore.utility;
 
 import java.security.SecureRandom;
+import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityUtility {
-    
-    @Bean
-    public static BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12, new SecureRandom());
-    }
-    
-    @Bean
-    public static String randomPassword() {
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
-        StringBuilder password = new StringBuilder();
-        SecureRandom secureRandom = new SecureRandom();
-        
-        while (password.length() < 20) {
-            int index = secureRandom.nextInt(SALTCHARS.length());
-            password.append(SALTCHARS.charAt(index));
-        }
-        
-        return password.toString();
-    }
+	
+	@Value("${security.password.strength:12}")
+	private int passwordStrength;
+	
+	@Bean
+	public static BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(12, new SecureRandom());
+	}
+	
+	@Bean
+	public static String randomPassword() {
+		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+		StringBuilder password = new StringBuilder();
+		SecureRandom rnd = new SecureRandom();
+		
+		while (password.length() < 20) {
+			int index = rnd.nextInt(SALTCHARS.length());
+			password.append(SALTCHARS.charAt(index));
+		}
+		
+		return password.toString();
+	}
+	
+	public static String generateSecureToken(int length) {
+		SecureRandom random = new SecureRandom();
+		byte[] bytes = new byte[length];
+		random.nextBytes(bytes);
+		
+		StringBuilder token = new StringBuilder();
+		for (byte b : bytes) {
+			token.append(String.format("%02x", b));
+		}
+		
+		return token.toString();
+	}
 }
