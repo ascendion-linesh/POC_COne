3912836c7b1773ac24129d311534eb8b9a5fccd3
ascendion-linesh <linesh.mishra@ascendion.com>
2026-02-04 12:31:19 +0530

    Latest remediated code commit

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -14,35 +14,41 @@ import com.bookstore.domain.security.UserRole;
 import com.bookstore.service.UserService;
 import com.bookstore.utility.SecurityUtility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class BookstoreApplication implements CommandLineRunner {
-    
-    @Autowired
-    private UserService userService;
+	
+	private static final Logger logger = LoggerFactory.getLogger(BookstoreApplication.class);
+	
+	@Autowired
+	private UserService userService;
 
-    public static void main(String[] args) {
-        SpringApplication.run(BookstoreApplication.class, args);
-    }
-    
-    @Override
-    public void run(String... args) throws Exception {
-        User existingUser = userService.findByUsername("j");
-        
-        if (existingUser == null) {
-            User user1 = new User();
-            user1.setFirstName("John");
-            user1.setLastName("Adams");
-            user1.setUsername("j");
-            user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
-            user1.setEmail("JAdams@gmail.com");
-            
-            Set<UserRole> userRoles = new HashSet<>();
-            Role role1 = new Role();
-            role1.setRoleId(1);
-            role1.setName("ROLE_USER");
-            userRoles.add(new UserRole(user1, role1));
-            
-            userService.createUser(user1, userRoles);
-        }
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		try {
+			User user1 = new User();
+			user1.setFirstName("John");
+			user1.setLastName("Adams");
+			user1.setUsername("j");
+			user1.setPassword(SecurityUtility.passwordEncoder().encode("p"));
+			user1.setEmail("JAdams@gmail.com");
+			
+			Set<UserRole> userRoles = new HashSet<>();
+			Role role1 = new Role();
+			role1.setRoleId(1);
+			role1.setName("ROLE_USER");
+			userRoles.add(new UserRole(user1, role1));
+			
+			userService.createUser(user1, userRoles);
+			logger.info("Default user created successfully");
+		} catch (Exception e) {
+			logger.error("Failed to create default user", e);
+		}
+	}
 }
